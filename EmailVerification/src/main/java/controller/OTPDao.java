83d549557729134c7bc045a75b05634656dabdee
Tanuj Rohilla <tanuj.rohilla@impetus.com>
2020-11-19 17:46:19 +0530

    updated

---
+++ EmailVerification/src/main/java/controller/OTPDao.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class OTPDao {
+    int otp;
+
+    public int getOtp() {
+        return otp;
+    }
+
+    public void setOtp(int otp) {
+        this.otp = otp;
+    }
+}
